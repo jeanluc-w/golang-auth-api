3762eef90a9f7896cd7f48a6fa57fe17975cbf13
Jean-Luc Williams <jeanluc.williams@proton.me>
2024-03-28 21:38:13 -0700

    Convert healthcheck response to JSON response object

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -7,7 +7,11 @@ import (
 // Simple API that writes a plain-text response with information about the
 // app status, environment (dev|stage|prod), and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Status: Available")
-	fmt.Fprintf(w, "Environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "Version: %s\n", version)
+	// Create the JSON body for the response
+	body := `{"status": "available", "environment": %q, "version": %q}`
+	body = fmt.Sprintf(body, app.config.env, version)
+	// Set the content-type header to JSON
+	w.Header().Set("Content-Type", "application/json")
+	// Write the JSON to the HTTP response body
+	w.Write([]byte(body))
 }
