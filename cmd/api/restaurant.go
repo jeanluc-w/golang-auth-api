9f759813a758cb77ad9c94ba3b675936c87581df
Jean-Luc Williams <jeanluc.williams@proton.me>
2024-03-28 21:18:56 -0700

    Add basic restaurant GET and POST endpoints

---
+++ cmd/api/restaurant.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "strconv" 
+  "github.com/julienschmidt/httprouter" 
+)
+
+
+func (app *application) createRestaurantHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "create a new restaurant")
+}
+
+
+func (app *application) getRestaurantHandler(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	// If the ByName() parameter couldn't be converted from a string to an int, 
+	// or is less than 1, we know the ID is invalid so return a 404 Not Found response.
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "show the details of restaurant %d\n", id)
+}
