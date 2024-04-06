894b4152349e2ad34eda446d498c30b1df210dad
Jean-Luc Williams <jeanluc.williams@proton.me>
2024-04-05 20:11:55 -0700

    Fix merge conflict

--- sql-migrations/local-db.sql
+++ sql-migrations/local-db.sql
@@ -1,2 +1,2 @@
-CREATE ROLE edibubble_admin LOGIN PASSWORD 'set_this_yourself';
+CREATE ROLE edibubble_admin LOGIN PASSWORD 'set_custom_password_here';
 CREATE DATABASE edibubble WITH OWNER = edibubble_admin;
