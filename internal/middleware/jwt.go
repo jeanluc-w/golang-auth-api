b1e31eae10a47069b650d864d5994a61ecf07dbc
Jean-Luc Williams <jeanluc.williams@proton.me>
2025-09-16 15:31:06 -0700

    Add start of handling refresh token requests

--- internal/middleware/jwt.go
+++ internal/middleware/jwt.go
@@ -25,18 +25,24 @@ func JWTMiddleware(redisClient *redis.Client) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ctx := r.Context()
-			// Ignore the routes where they are open to anyone
-			if isAlternateRoute(r.URL.Path, server.OpenRoutes) {
+			requestPath := r.URL.Path
+			// Open Routes: Ignore the routes where they are open to anyone
+			if isAlternateRoute(requestPath, server.OpenRoutes) {
 				next.ServeHTTP(w, r)
 				return
 			}
 
 			// If not open, get the token and validate.
 			authHeader := r.Header.Get("Authorization")
-			utils.LogDebug(ctx, "ROUTES", zap.Any("routes", server.TemporaryJWTRoutes), zap.Any("path", r.URL.Path))
+			utils.LogDebug(ctx, "ROUTES", zap.Any("routes", server.TemporaryJWTRoutes), zap.Any("path", requestPath))
 
-			// Verify the Temporary JWT if the requested route is part of the temporary JWT validation routes
-			if isAlternateRoute(r.URL.Path, server.TemporaryJWTRoutes) {
+			// TODO: Implement different Auth check if it's the token refresh route since the token should be expired.
+			if requestPath == server.V1_RefreshToken {
+
+			}
+
+			// Temporary JWT Routes: Verify the Temporary JWT with different rules if the
+			if isAlternateRoute(requestPath, server.TemporaryJWTRoutes) {
 				email, id, err := auth.VerifyTemporaryJWT(ctx, redisClient, authHeader)
 				if err != nil {
 					utils.LogDebug(ctx, "Temporary JWT validation failed", zap.Error(err))
@@ -49,7 +55,7 @@ func JWTMiddleware(redisClient *redis.Client) func(http.Handler) http.Handler {
 				return
 			}
 
-			// Verify the JWT token and get back the user object if successful
+			// Verify the JWT token like normal and get back the user object if successful
 			user, err := auth.VerifyAndParseJWT(ctx, redisClient, authHeader)
 			if err != nil {
 				utils.LogDebug(ctx, "JWT validation failed", zap.Error(err))
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -8,6 +8,7 @@ const (
 	V1_StartEmailVerification string = "/auth/v1/start-email-verification"
 	V1_VerifyEmail            string = "/auth/v1/verify-email"
 	V1_CompleteEmailJoin      string = "/auth/v1/complete-email-join"
+	V1_RefreshToken           string = "/auth/v1/refresh-token"
 	// Forgot password
 	// Change Password
 	// SSO
